01b1d00977e24fdd2eb99eec1a2cb085c6fa948f
Matthew Henrich <matthewhenrich04@gmail.com>
2026-05-05 19:35:30 +0800

    Fix some bug

--- app.py
+++ app.py
@@ -2,45 +2,43 @@ import streamlit as st
 from core.decrypt import Decrypt
 from core.encrypt import Encrypt
 
-class HashUI:
-    def __init__(self):
-        st.title("Hashman 🔐#️⃣")
-        st.markdown("<h5>Hashman - Your secret vault for your message, stored in URL 🔗</h5>", unsafe_allow_html=True)
+st.title("Hashman 🔐#️⃣")
+st.markdown("<h5>Hashman - Your secret vault for your message, stored in URL 🔗</h5>", unsafe_allow_html=True)
 
-        self.query_params = st.query_params
-        self.secret_data = self.query_params.get("data")
-        self.encrypt = Encrypt()
-        self.decrypt = Decrypt()
+query_params = st.query_params
+secret_data = query_params.get("data")
+encrypt = Encrypt()
+decrypt = Decrypt()
 
-        if self.secret_data:
-            self.viewing_mode()
+def viewing_mode():
+    st.info("A Secret Message has been sent to you")
+    inputted_pwd = st.text_input("Input password to decrypt", key="decrypt_pwd", type="password")
+
+    if st.button("Unlock"):
+        decrypted = decrypt.decrypt_message(secret_data, inputted_pwd)
+        if decrypted:
+            st.toast("Message Decrypted!", icon="✅")
+            st.code(decrypt, language=None)
         else:
-            self.creation_mode()
+            st.toast("Incorrect Password or Corrupted Link. Try Again!", icon="‼️")
 
-    def viewing_mode(self):
-        st.info("A Secret Message has been sent to you")
-        inputted_pwd = st.text_input("Input password to decrypt", key="decrypt_pwd", type="password")
+def creation_mode():
+    st.subheader("Create a Secret")
+    msg = st.text_input("Your Message", key="secret_msg")
+    pwd = st.text_input("Password", key="secret_pwd", type="password")
 
-        if st.button("Unlock"):
-            decrypted = self.decrypt.decrypt_message(self.secret_data, inputted_pwd)
-            if decrypted:
-                st.toast("Message Decrypted!", icon="✅")
-                st.code(self.decrypt, language=None)
-            else:
-                st.toast("Incorrect Password or Corrupted Link. Try Again!", icon="‼️")
+    if st.button("Generate Secret Link"):
+        if msg and pwd:
+            encrypted = encrypt.encrypt_message(msg, pwd)
+            base_url = "https://hashman-code.streamlit.app"
+            shared_link = f"{base_url}/?data={encrypted}"
 
-    def creation_mode(self):
-        st.subheader("Create a Secret")
-        msg = st.text_input("Your Message", key="secret_msg")
-        pwd = st.text_input("Password", key="secret_pwd", type="password")
+            st.write("Copy this link and share to a friend")
+            st.code(shared_link)
+        else:
+            st.toast("Please Enter both Message and Password", icon="‼️")
 
-        if st.button("Generate Secret Link"):
-            if msg and pwd:
-                encrypted = self.encrypt.encrypt_message(msg, pwd)
-                base_url = "https://hashman-code.streamlit.app"
-                shared_link = f"{base_url}/?data={encrypted}"
-
-                st.write("Copy this link and share to a friend")
-                st.code(shared_link)
-            else:
-                st.toast("Please Enter both Message and Password", icon="‼️")
+if secret_data:
+    viewing_mode()
+else:
+    creation_mode()
